f88141739226276a91b292515f21231d81577545
ian wairimu <wairimuianmoon@gmail.com>
2022-09-17 16:55:42 +0300

    created the account test

--- db/query/account.sql
+++ db/query/account.sql
@@ -15,7 +15,11 @@ ORDER BY id
 LIMIT $1
 OFFSET  $2;
 
--- update: UpdateAccounts :exec
+-- name: UpdateAccounts :exec
 UPDATE accounts
 SET balance = $2
+WHERE id = $1;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
 WHERE id = $1;
